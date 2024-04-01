05fcd33d1d8daecd20bbc8c9f3f8730c09b16eae
Carl Perez <carsplanesss@gmail.com>
2024-04-02 01:13:13 +0800

    cart and "checkout"

--- BUUKSTUR/storeForm.Designer.cs
+++ BUUKSTUR/storeForm.Designer.cs
@@ -30,62 +30,81 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(storeForm));
             dgvBooks = new DataGridView();
-            btnLoad = new Button();
             btnLogout = new Button();
+            dgvCart = new DataGridView();
+            btnCheckout = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvBooks).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCart).BeginInit();
             SuspendLayout();
             // 
             // dgvBooks
             // 
             dgvBooks.BackgroundColor = Color.FromArgb(255, 192, 128);
             dgvBooks.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvBooks.Location = new Point(130, 35);
+            dgvBooks.Location = new Point(61, 35);
             dgvBooks.Name = "dgvBooks";
             dgvBooks.RowTemplate.Height = 25;
-            dgvBooks.Size = new Size(679, 316);
+            dgvBooks.Size = new Size(585, 330);
             dgvBooks.TabIndex = 0;
-            // 
-            // btnLoad
-            // 
-            btnLoad.Location = new Point(130, 371);
-            btnLoad.Name = "btnLoad";
-            btnLoad.Size = new Size(75, 23);
-            btnLoad.TabIndex = 1;
-            btnLoad.Text = "Load";
-            btnLoad.UseVisualStyleBackColor = true;
-            btnLoad.Click += btnLoad_Click;
             // 
             // btnLogout
             // 
-            btnLogout.Location = new Point(211, 371);
+            btnLogout.BackColor = Color.IndianRed;
+            btnLogout.BackgroundImageLayout = ImageLayout.Stretch;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.ForeColor = Color.White;
+            btnLogout.Location = new Point(848, 501);
             btnLogout.Name = "btnLogout";
-            btnLogout.Size = new Size(75, 23);
+            btnLogout.Size = new Size(75, 33);
             btnLogout.TabIndex = 2;
             btnLogout.Text = "Logout";
-            btnLogout.UseVisualStyleBackColor = true;
+            btnLogout.UseVisualStyleBackColor = false;
             btnLogout.Click += btnLogout_Click;
+            // 
+            // dgvCart
+            // 
+            dgvCart.BackgroundColor = Color.FromArgb(255, 192, 128);
+            dgvCart.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCart.Location = new Point(699, 35);
+            dgvCart.Name = "dgvCart";
+            dgvCart.RowTemplate.Height = 25;
+            dgvCart.Size = new Size(206, 330);
+            dgvCart.TabIndex = 3;
+            // 
+            // btnCheckout
+            // 
+            btnCheckout.FlatStyle = FlatStyle.Flat;
+            btnCheckout.Location = new Point(761, 383);
+            btnCheckout.Name = "btnCheckout";
+            btnCheckout.Size = new Size(85, 33);
+            btnCheckout.TabIndex = 4;
+            btnCheckout.Text = "Checkout";
+            btnCheckout.UseVisualStyleBackColor = true;
             // 
             // storeForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(894, 511);
+            ClientSize = new Size(935, 546);
+            Controls.Add(btnCheckout);
+            Controls.Add(dgvCart);
             Controls.Add(btnLogout);
-            Controls.Add(btnLoad);
             Controls.Add(dgvBooks);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "storeForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "BUUKSTUR";
             ((System.ComponentModel.ISupportInitialize)dgvBooks).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCart).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private DataGridView dgvBooks;
-        private Button btnLoad;
         private Button btnLogout;
+        private DataGridView dgvCart;
+        private Button btnCheckout;
     }
 }
--- BUUKSTUR/storeForm.cs
+++ BUUKSTUR/storeForm.cs
@@ -19,23 +19,109 @@ namespace BUUKSTUR
         OleDbCommand? cmd;
         DataSet? ds;
         int indexRow;
+        private BindingList<CartItem> cartItems = new BindingList<CartItem>();
         public storeForm()
         {
             InitializeComponent();
-
+            this.Load += new EventHandler(storeForm_Load);
+            SetupBooksDataGridView();
+            dgvCart.AutoGenerateColumns = true;
+            dgvCart.DataSource = cartItems;
+            this.dgvBooks.CellContentClick += new DataGridViewCellEventHandler(this.dgvBooks_CellContentClick);
+            this.btnCheckout.Click += new EventHandler(this.btnCheckout_Click);
 
         }
-
-        private void btnLoad_Click(object sender, EventArgs e)
+        private void SetupBooksDataGridView()
         {
-            myConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\Administrator\\Documents\\OOP2\\buuksturr.mdb\"");
-            da = new OleDbDataAdapter("SELECT *FROM Inventory", myConn);
-            ds = new DataSet();
-            myConn.Open();
-            da.Fill(ds, "Inventory");
-            dgvBooks.DataSource = ds.Tables["Inventory"];
-            myConn.Close();
+            DataGridViewButtonColumn addToCartButton = new DataGridViewButtonColumn
+            {
+                Name = "addToCartButton",
+                HeaderText = "Add to Cart",
+                Text = "Add",
+                UseColumnTextForButtonValue = true
+            };
+            dgvBooks.Columns.Add(addToCartButton);
         }
+        private void LoadBooks()
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\Administrator\\Documents\\OOP2\\buuksturr.mdb\"";
+            string query = "SELECT * FROM Inventory";
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection))
+            {
+                DataTable books = new DataTable();
+                adapter.Fill(books);
+                dgvBooks.DataSource = books;
+            }
+        }
+
+        private void storeForm_Load(object sender, EventArgs e)
+        {
+            LoadBooks(); // Call a method to load the books
+        }
+
+        public class CartItem
+        {
+            public int BookID { get; set; }
+            public string Title { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+        }
+        
+
+        private void AddToCart(int bookId, string title, decimal price)
+        {
+            var item = cartItems.FirstOrDefault(i => i.BookID == bookId);
+            if (item != null)
+            {
+                item.Quantity += 1;
+            }
+            else
+            {
+                cartItems.Add(new CartItem { BookID = bookId, Title = title, Price = price, Quantity = 1 });
+            }
+        }
+
+        private void UpdateCartUI()
+        {
+            //dgvCart.DataSource = typeof(List<>);
+            dgvCart.DataSource = cartItems;
+            dgvCart.Refresh();
+        }
+        private void dgvBooks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvBooks.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
+            {
+                var row = dgvBooks.Rows[e.RowIndex];
+                int bookId = Convert.ToInt32(row.Cells["BOOKID"].Value);
+                string title = row.Cells["Title"].Value.ToString();
+                decimal price = Convert.ToDecimal(row.Cells["Price"].Value);
+                AddToCart(bookId, title, price);
+            }
+        }
+
+
+        private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            if (cartItems.Count > 0)
+            {
+                var confirmResult = MessageBox.Show("Are you sure you want to checkout these items?",
+                                                     "Confirm Checkout",
+                                                     MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.Yes)
+                {
+                    cartItems.Clear();
+                    UpdateCartUI();
+
+                    MessageBox.Show("Checkout successful.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Your cart is empty.");
+            }
+        }
+        
 
         private void btnLogout_Click(object sender, EventArgs e)
         {
@@ -66,6 +152,7 @@ namespace BUUKSTUR
                 Application.Exit();
             }
         }
+        
 
 
     }
